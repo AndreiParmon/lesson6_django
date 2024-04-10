5a053188fbc06d0df0fb30850006e8ba3e61e6f8
AndreiParmon <parmonandrei@gmail.com>
2024-04-10 21:45:51 +0300

    changes

--- lesson_2_seminar/settings.py
+++ lesson_2_seminar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'f0862e812bf7a852dc4b91df8295d5fdc28b927ae58f11e3ba9267de86f4a82e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get("DEBUG") == "False":
